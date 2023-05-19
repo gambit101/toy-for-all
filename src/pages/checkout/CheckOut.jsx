01974d6e7924cfbfd49370ab653dbb0f09a3376d
gambit101 <mohammadsamraj@gmail.com>
2023-05-19 12:16:54 +0600

    added login and register

---
+++ src/pages/checkout/CheckOut.jsx
@@ -0,0 +1,89 @@
+import { useContext } from "react";
+import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
+
+const CheckOut = () => {
+
+    const categories = useLoaderData();
+
+    const { title, _id, price, category, quantity, img } = categories;
+    const {user} = useContext(AuthContext)
+
+    const handleBookService = () =>{
+        event.preventDefault();
+
+        const form = event.target;
+        const name = form.name.value;
+        const email = user?.email;
+        const category = form.category.value;
+
+        const booking = {
+            customerName: name,
+            email,
+            img,
+            category,
+            service: title,
+            service_id: _id,
+            price: price,
+            quantity
+        }
+        console.log(booking);
+    }
+
+    return (
+        <div>
+            <h2 className="text-3xl text-center my-8 font-bold">Toy Name: {title}</h2>
+            <img className="ml-96 my-8 rounded-xl" src={img} alt="" />
+
+            <form onSubmit={handleBookService}>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input type="text" name='name' placeholder="Name"  className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Category</span>
+                        </label>
+                        <input type="text" placeholder="category" defaultValue={category} name='category' className="input input-bordered" />
+
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="text" name='email' defaultValue={user?.email}  placeholder="email" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Due amount</span>
+                        </label>
+                        <input type="text" defaultValue={price} className="input input-bordered" />
+
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Available quantity</span>
+                        </label>
+                        <input type="text" defaultValue={quantity} className="input input-bordered" />
+
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Description</span>
+                        </label>
+                        <input type="text" placeholder="Description" className="input input-bordered" />
+
+                    </div>
+                </div>
+                <div className="form-control mt-6">
+                    <input type="submit" className="btn btn-primary" value="Order confirm" />
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default CheckOut;
--- src/routs/Routs.jsx
+++ src/routs/Routs.jsx
@@ -4,6 +4,7 @@ import Home from "../pages/home/home/Home";
 import Login from "../pages/login/Login";
 import SignUp from "../pages/signUp/SignUp";
 import AddToy from "../pages/addToy/AddToy";
+import CheckOut from "../pages/checkout/CheckOut";
 
 const router = createBrowserRouter([
     {
@@ -25,6 +26,11 @@ const router = createBrowserRouter([
         {
           path: 'addToy',
           element: <AddToy></AddToy>
+        },
+        {
+          path: 'checkOut/:id',
+          element: <CheckOut></CheckOut>,
+          loader: ({params}) => fetch(`http://localhost:5000/category/${params.id}`)
         }
       ]
     },
